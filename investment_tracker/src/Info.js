5ec175cc40203c6d7fbff23be61c899f04069daa
lachiep9 <npal0003@student.monash.edu>
2023-07-13 17:37:50 +1000

    initial tutorial application - inventory

---
+++ investment_tracker/src/Info.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+class Info extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+    };
+    // this.buttonPressed = this.buttonPressed.bind(this);
+  }
+
+  buttonPressed() {
+    this.setState({
+      count: this.state.count + 1,
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <p>Count: {this.state.count}</p>;
+        <button onClick={() => this.buttonPressed()}>Click Me!</button>
+      </div>
+    );
+  }
+}
+
+export default Info;
